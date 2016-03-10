b4c0ef5970a435d8c57c2bc15e274ee37bcfaa4b
marvin <marvin@greeny.cs.tlu.ee>
2016-03-10 11:36:22 +0200

    added deleting option to homework

--- table2.php
+++ table2.php
@@ -7,9 +7,35 @@ require_once("../../config.php");
 //create connection_aborted
 $mysql = new mysqli("localhost",$db_username, $db_password,"webpr2016_marvin");
 
+
+/*
+IF THERE IS ?DELETE=ROW_ID in the url
+*/
+if(isset($_GET["delete"])){
+	
+	echo "Deleting row with id:".$_GET["delete"];
+	
+	$stmt = $mysql->prepare("UPDATE BMI_app SET deleted=NOW() WHERE id = ?");
+	
+	echo $mysql->error;
+	
+	//replace the ?
+	$stmt->bind_param("i", $_GET["delete"]);
+	
+	if($stmt->execute()){
+		echo "deleted successfully";
+	}else{
+		echo $stmt->error;
+	}
+	
+	//closes the statement, so others can use connection
+	$stmt->close();
+}
+
+
 //SQL sentence
 $stmt = $mysql->prepare("SELECT id, gender, age, height, weight,
- created FROM BMI_app ORDER BY id DESC LIMIT 10");
+ created FROM BMI_app WHERE deleted IS NULL ORDER BY id DESC LIMIT 10");
 
 //if error in sentence
 echo $mysql->error;
@@ -30,6 +56,7 @@ $table2_html .="<th>Age</th>";
 $table2_html .="<th>Height</th>";
 $table2_html .="<th>Weight</th>";
 $table2_html .="<th>Created</th>";
+$table2_html .="<th>Delete?</th>";
 $table2_html .="</tr>";
 		
 //GET RESULT
@@ -45,6 +72,7 @@ $table2_html .="<td>".$age."</td>";
 $table2_html .="<td>".$height."</td>";
 $table2_html .="<td>".$weight."</td>";
 $table2_html .="<td>".$created."</td>";
+$table2_html .="<td><a href='?delete=".$id."'>X</a></td>";
 $table2_html .="</tr>"; //end row
 	
 	
